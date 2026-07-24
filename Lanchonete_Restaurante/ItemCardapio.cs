ffcab07c0d811003051b5a11f512d395dfa65013
moc <moc@DESKTOP-AN8V6J3>
2026-07-23 21:44:38 -0300

    Adicionar arquivos de projeto.

---
+++ Lanchonete_Restaurante/ItemCardapio.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lanchonete_Restaurante
+{
+    internal class ItemCardapio
+    {
+        public int Id { get; private set; }
+        public string Nome { get; set; }
+        public string Categoria { get; set; }
+        public decimal PrecoBase { get; set; }
+        public bool EstaDisponivel { get; private set; }
+
+        public ItemCardapio(int id, string nome, string categoria, decimal precoBase, bool estaDisponivel)
+        {
+            if (precoBase <= 0)
+                throw new ArgumentException("O preço base não pode ser igual a zero ou negativo.");
+            this.Id = id;
+            this.Nome = nome;
+            this.Categoria = categoria;
+            this.PrecoBase = precoBase;
+            this.EstaDisponivel = estaDisponivel;
+        }
+
+        public void PausarVendas()
+        {
+            this.EstaDisponivel = false;
+            return;
+        }
+
+        public void ReativarVendas()
+        {
+            this.EstaDisponivel = true;
+            return;
+        }
+
+        public void AplicarDesconto(decimal porcentagem)
+        {
+            if (porcentagem <= 0 || porcentagem > 30)
+            {
+                throw new ArgumentException("O desconto informado não é aceito.");
+            }
+            else
+            {
+                this.PrecoBase -= (this.PrecoBase*(porcentagem / 100));
+            }
+        }
+    }
+}
---
+++ Lanchonete_Restaurante/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lanchonete_Restaurante
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<ItemCardapio> listaCardapio = new List<ItemCardapio>();
+
+            while (true)
+            {
+                Console.WriteLine("1 - Cadastrar Item: ");
+                Console.WriteLine("2 - Listar Cardapio: ");
+                Console.WriteLine("3 - Alterar Preço / Aplicar Desonto: ");
+                Console.WriteLine("4 - Pausar / Reativar Vendas: ");
+                Console.WriteLine("5 - Remover Item: ");
+                Console.WriteLine("6 - Sair.");
+               
+                int menu = int.Parse(Console.ReadLine());
+
+                if (menu == 0)
+                    Console.WriteLine("Saindo..."); break;
+
+                switch (menu)
+                {
+
+                }
+
+                
+            }
+        }
+    }
+}
